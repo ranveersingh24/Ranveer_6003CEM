ebddb9f2dfcaaa1138c5424eb40e4bf4fc9154de
ranveersingh24 <p20012436@student.newinti.edu.my>
2023-06-30 20:40:13 +0800

    Commit

--- src/ConnectServer/Express.js
+++ src/ConnectServer/Express.js
@@ -25,84 +25,17 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-// Register a new user
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-
-  // Create a new user object
-  const newUser = new User({ username, password });
-
-  // Save the user to the database
-  newUser.save((error) => {
-    if (error) {
-      console.log('Error saving user:', error);
-      return res.status(500).send('Error registering user');
-    }
-    res.status(200).send('Registration successful');
-  });
-});
-
-const apikey = 'fc1d5233';
-
-const registeredUsers = []; // Store registered users
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Set up HTML layout
-const htmlLayout = (title, content) => `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>${title}</title>
-    </head>
-    <body>
-      <h1>${title}</h1>
-      ${content}
-    </body>
-  </html>
-`;
-
-// Home page
+// Register and Login page
 app.get('/', (req, res) => {
-  const content = `
-    <p>Welcome to the Movie API homepage!</p>
-    <ul>
-      <li><a href="/getAllMovie">Get All Movies</a></li>
-      <li><a href="/getMovie">Get a Movie</a></li>
-      <li><a href="/updateMovie">Update a Movie</a></li>
-      <li><a href="/deleteMovie">Delete a Movie</a></li>
-      <li><a href="/deleteAllMovie">Delete All Movies</a></li>
-      <li><a href="/login">Login</a></li>
-      <li><a href="/register">Register</a></li>
-    </ul>
-  `;
-  res.send(htmlLayout("Movie API", content));
-});
-
-// Login page
-app.get('/login', (req, res) => {
   res.send(`
-    <h2>Login</h2>
+    <h2>Login Page</h2>
     <form action="/login" method="POST">
       <label for="username">Username:</label>
       <input type="text" id="username" name="username" /><br />
       <label for="password">Password:</label>
       <input type="password" id="password" name="password" /><br />
       <button type="submit">Login</button>
-    </form>
-  `);
-});
-
-// Register page
-app.get('/register', (req, res) => {
-  res.send(`
-    <h2>Register</h2>
-    <form action="/register" method="POST">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" /><br />
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" /><br />
-      <button type="submit">Register</button>
+      <p>Don't have an account? Click here to register now <button type="button" onclick="window.location.href='/register'">Register</button>
     </form>
   `);
 });
@@ -128,26 +61,79 @@ app.post('/login', (req, res) => {
     }
 
     // Successful login
-    res.status(200).send('Login successful');
+    const content = `
+      <p>Welcome, ${username}! <br><a href="/homepage">Go to homepage</a></p>
+    `;
+    res.send(htmlLayout("Movie API", content));
   });
 });
 
-
-// Handle register form submission
+// Register
 app.post('/register', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
 
-  // Check if the user is already registered
-  const existingUser = registeredUsers.find((user) => user.username === username);
-  if (existingUser) {
-    res.send('Username already taken. Please choose a different username.');
-  } else {
-    registeredUsers.push({ username, password });
-    res.send('Registration successful');
-  }
+  // Create a new user object
+  const newUser = new User({ username, password });
+
+  // Save the user to the database and checks for existing users
+  newUser.save((error) => {
+    if (error) {
+      console.log('Error saving user:', error);
+      return res.status(500).send('Error registering user');
+    }
+    res.status(200).send('Registration successful! <br> Click here to login now <a href="/">Login</a>');
+  });
 });
 
+const apikey = 'fc1d5233';
+const registeredUsers = []; // Store registered users
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up HTML layout
+const htmlLayout = (title, content) => `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>${title}</title>
+    </head>
+    <body>
+      <h1>${title}</h1>
+      ${content}
+    </body>
+  </html>
+`;
+
+// Home page
+app.get('/homepage', (req, res) => {
+  const content = `
+    <p>Welcome to the Movie API homepage!</p>
+    <ul>
+      <li><a href="/getAllMovie">Get All Movies</a></li>
+      <li><a href="/getMovie">Get a Movie</a></li>
+      <li><a href="/updateMovie">Update a Movie</a></li>
+      <li><a href="/deleteMovie">Delete a Movie</a></li>
+      <li><a href="/deleteAllMovie">Delete All Movies</a></li>
+    </ul>
+    <p><a href="/">Logout</a></p>
+  `;
+  res.send(htmlLayout("Movie API", content));
+});
+
+// Register page
+app.get('/register', (req, res) => {
+  res.send(`
+    <h2>Register Page</h2>
+    <form action="/register" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" /><br />
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" /><br />
+      <button type="submit">Register</button>
+      <p>Already have an account? Click here to login now <button type="button" onclick="window.location.href='/'">Login</button>
+    </form>
+  `);
+});
 
 // Get all movies
 app.get('/getAllMovie', (req, res) => {
@@ -156,7 +142,7 @@ app.get('/getAllMovie', (req, res) => {
       console.log("Error retrieving movies: " + err);
       const content = `
         <p>Error retrieving movies from the database</p>
-        <p><a href="/">Go back to homepage</a></p>
+        <p><a href="/homepage">Go back to homepage</a></p>
       `;
       res.status(500).send(htmlLayout("Get All Movies", content));
     } else {
@@ -168,7 +154,7 @@ app.get('/getAllMovie', (req, res) => {
       const content = `
         <h2>All Movies</h2>
         <ul>${movieList}</ul>
-        <p><a href="/">Go back to homepage</a></p>
+        <p><a href="/homepage">Go back to homepage</a></p>
       `;
       res.send(htmlLayout("Get All Movies", content));
     }
@@ -226,7 +212,7 @@ app.get('/getMovie', (req, res) => {
           <p>Plot: ${Plot}</p>
           <img src="${Poster}" alt="${Title} Poster" width="200" />
           <p>Record saved</p>
-          <p><a href="/">Go back to homepage</a></p>
+          <p><a href="/homepage">Go back to homepage</a></p>
         `;
 
         res.send(htmlLayout("Get a Movie", content));
@@ -277,7 +263,7 @@ app.get('/updateMovie', (req, res) => {
           console.log("Error updating movie: " + err);
           const content = `
             <p>Error updating movie in the database</p>
-            <p><a href="/">Go back to homepage</a></p>
+            <p><a href="/homepage">Go back to homepage</a></p>
           `;
           res.status(500).send(htmlLayout("Update a Movie", content));
         } else {
@@ -285,7 +271,7 @@ app.get('/updateMovie', (req, res) => {
             const content = `
               <p>No movie found with the title "${title}"</p>
               <p><a href="/updateMovie">Try again</a></p>
-              <p><a href="/">Go back to homepage</a></p>
+              <p><a href="/homepage">Go back to homepage</a></p>
             `;
             res.send(htmlLayout("Update a Movie", content));
           } else {
@@ -295,7 +281,7 @@ app.get('/updateMovie', (req, res) => {
               <p>Genre: ${updatedMovie.movieGenre}</p>
               <p>Plot: ${updatedMovie.moviePlot}</p>
               <p><a href="/updateMovie">Update another movie</a></p>
-              <p><a href="/">Go back to homepage</a></p>
+              <p><a href="/homepage">Go back to homepage</a></p>
             `;
             res.send(htmlLayout("Update a Movie", content));
           }
@@ -331,7 +317,7 @@ app.get('/deleteMovie', (req, res) => {
 
     const content = `
       <p>${title} deleted</p>
-      <p><a href="/">Go back to homepage</a></p>
+      <p><a href="/homepage">Go back to homepage</a></p>
     `;
     res.send(htmlLayout("Delete a Movie", content));
   }
@@ -344,14 +330,14 @@ app.get('/deleteAllMovie', (req, res) => {
       console.log("Error deleting documents: " + err);
       const content = `
         <p>Error deleting documents from the database</p>
-        <p><a href="/">Go back to homepage</a></p>
+        <p><a href="/homepage">Go back to homepage</a></p>
       `;
       res.status(500).send(htmlLayout("Delete All Movies", content));
     } else {
       console.log("All documents deleted successfully");
       const content = `
         <p>All movies deleted</p>
-        <p><a href="/">Go back to homepage</a></p>
+        <p><a href="/homepage">Go back to homepage</a></p>
       `;
       res.send(htmlLayout("Delete All Movies", content));
     }
