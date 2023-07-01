7d747edde53c1f839c542fe3aa6c1f0730eb470c
ranveersingh24 <103876712+ranveersingh24@users.noreply.github.com>
2023-07-01 23:37:59 +0800

    Add files via upload

---
+++ src/MovieList.js
@@ -0,0 +1,10 @@
+import { Movie } from "./Movie";
+export function MovieList({ onSelectMovie, movies }) {
+  return (
+    <ul className="list list-movies">
+      {movies?.map((movie) => (
+        <Movie onSelectMovie={onSelectMovie} movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
